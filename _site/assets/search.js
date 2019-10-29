6ef386dc3187fb11b8544207c70c01d0810bae0d
Di Peng <di.peng1@student.unsw.edu.au>
2019-10-29 23:01:55 +1100

    first add

--- _site/assets/search.js
+++ _site/assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"Post with Header Image",'url':"/2018/06/01/header-image.html"},{'title':"Welcome",'url':"/2018/07/01/welcome.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"操作系统知识点整理 (一)",'url':"/2019/10/29/%E6%93%8D%E4%BD%9C%E7%B3%BB%E7%BB%9F%E7%9F%A5%E8%AF%86%E7%82%B9%E6%95%B4%E7%90%86.html"}]};
